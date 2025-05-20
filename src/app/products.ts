e9ad7bb3cc807b14abc6a4a9b68ac21d696d9061
umi-gt-hb01 <gt-hb01@hima-sv.wjg.jp>
2025-05-21 08:33:30 +0900

    teiki 20250521

---
+++ src/app/products.ts
@@ -0,0 +1,58 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    heading1: 'Phone XL1',
+    heading2: 'Phone XL2',
+    heading3: 'Phone XL3',
+    headingText1:
+      'heading Text Phone XL1 heading Text Phone XL1 heading Text Phone XL1 heading Text Phone XL1 heading Text Phone XL1',
+    headingText2:
+      'heading Text Phone XL2 heading Text Phone XL2 heading Text Phone XL2 heading Text Phone XL2 heading Text Phone XL2',
+    headingText3:
+      'heading Text Phone XL3 heading Text Phone XL3 heading Text Phone XL3 heading Text Phone XL3 heading Text Phone XL3',
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    heading1: 'Phone Mini1',
+    heading2: 'Phone Mini2',
+    heading3: 'Phone Mini3',
+    headingText1:
+      'heading Text Phone Mini1 heading Text Phone Mini1 heading Text Phone Mini1 heading Text Phone Mini1 heading Text Phone Mini1',
+    headingText2:
+      'heading Text Phone Mini2 heading Text Phone Mini2 heading Text Phone Mini2 heading Text Phone Mini2 heading Text Phone Mini2',
+    headingText3:
+      'heading Text Phone Mini3 heading Text Phone Mini3 heading Text Phone Mini3 heading Text Phone Mini3 heading Text Phone Mini3',
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: 'description Phone Standard',
+    heading1: 'Phone Standard1',
+    heading2: 'Phone Standard2',
+    heading3: 'Phone Standard3',
+    headingText1:
+      'heading Text Phone Standard1 heading Text Phone Standard1 heading Text Phone Standard1 heading Text Phone Standard1 heading Text Phone Standard1',
+    headingText2:
+      'heading Text Phone Standard2 heading Text Phone Standard2 heading Text Phone Standard2 heading Text Phone Standard2 heading Text Phone Standard2',
+    headingText3:
+      'heading Text Phone Standard3 heading Text Phone Standard3 heading Text Phone Standard3 heading Text Phone Standard3 heading Text Phone Standard3',
+  },
+  {
+    name: 'Phone Special',
+    price: 999,
+    description: 'description Phone Special',
+    heading1: 'Phone Special1',
+    heading2: 'Phone Special2',
+    heading3: 'Phone Special3',
+    headingText1:
+      'heading Text Phone Special1 heading Text Phone Special1 heading Text Phone Special1 heading Text Phone Special1 heading Text Phone Special1',
+    headingText2:
+      'heading Text Phone Special2 heading Text Phone Special2 heading Text Phone Special2 heading Text Phone Special2 heading Text Phone Special2',
+    headingText3:
+      'heading Text Phone Special3 heading Text Phone Special3 heading Text Phone Special3 heading Text Phone Special3 heading Text Phone Special3',
+  },
+];
